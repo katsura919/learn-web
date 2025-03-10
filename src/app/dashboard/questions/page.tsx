4aae57544ddfeb3914df6e86033cf432042bde26
Jan Paul <katsuragik919@gmail.com>
2025-03-10 23:07:24 +0800

    test

---
+++ src/app/dashboard/questions/page.tsx
@@ -0,0 +1,24 @@
+"use client";
+import { useAuth } from "@/context/AuthContext";
+import LessonList from "@/components/home/lesson-list";
+import { motion } from "framer-motion"; 
+
+export default function QuestionPage() {
+  const { logout } = useAuth();
+
+  return (
+    <div className="relative flex flex-col items-center justify-center min-h-screen w-full p-6">
+      {/* Lesson List Container */}
+      <motion.div
+        initial={{ opacity: 0, scale: 0.95 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{ duration: 0.4 }}
+        className="w-full h-full flex flex-col flex-grow"
+      >
+       <div>
+        question
+       </div>
+      </motion.div>
+    </div>
+  );
+}
